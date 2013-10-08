9713e19e426f4b2cfb2f8a857e64b008de05e28b
Lioshaq <alexei.melnic@gmail.com>
2013-10-08 18:47:48 +0300

    Page titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,11 @@ describe "StaticPages" do
     it "should have the content 'sample app'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-	    	page.should have_content('Sample App')     
+	    	page.should have_selector('h1', :text => 'Sample App')
+		end
+		it "should have the title Home" do
+			visit '/static_pages/home'
+			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
 		end
   end
 	
